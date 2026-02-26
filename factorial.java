765a528c05790569dad915fcddc5662001cf97c2
ShankhaSubhraDatta <duttashankhasubhra@gmail.com>
2026-02-26 10:00:44 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Factorial {
+
+    // Member function to calculate factorial
+    int calculateFactorial(int n) {
+        int fact = 1;
+
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        Factorial obj = new Factorial();   // Creating object
+        int result = obj.calculateFactorial(num);  // Calling member function
+
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+}
